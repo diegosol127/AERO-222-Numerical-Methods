20feda52ea450e0324bf7c808187d71b75f3839b
diegosol127 <diegosol127@gmail.com>
2023-01-28 19:24:57 -0600

    create template for Lab2

---
+++ Lab/Lab2/AERO_222_Lab_2_template.py
@@ -0,0 +1,149 @@
+# Import libraries
+import numpy as np
+import matplotlib.pyplot as plt
+from math import log2, ceil
+
+# # Find the Root of the Function
+# Let's start by looking at a sample function and plotting it
+
+# original function
+def f(x):
+    return 0
+
+# first derivative
+def df(x):
+    return 0
+
+# second derivative
+def ddf(x):
+    return 0
+
+# # Bisection Method
+
+# Bisection method algorithm
+def bisection():
+    #==================================================================
+    # INPUTS
+    # -
+    # OUTPUTS
+    # -
+    #==================================================================
+
+    print('*** BISECTION METHOD ***')
+    return 0
+
+# How many iterations will it take to converge?
+def bisectionNumIterations():
+    #==================================================================
+    # INPUTS
+    # -
+    # OUTPUT
+    # -
+    #==================================================================
+    return 0
+
+# Test the algorithm and plot it
+
+
+# # Secant Method
+
+# Secant method algorithm
+def secant():
+    #==================================================================
+    # INPUTS
+    # -
+    # OUTPUTS
+    # -
+    #==================================================================
+
+    print('*** SECANT METHOD ***')
+    return 0
+
+# Test the algorithm and plot it
+
+
+# # Regula-Falsi Method
+
+# Regula-Falsi method algorithm
+def regula_falsi():
+    #==================================================================
+    # INPUTS
+    # -
+    # OUTPUTS
+    # -
+    #==================================================================
+
+    print('*** REGULA-FALSI METHOD ***')
+    return 0
+
+# Test the algorithm and plot it
+
+
+# # Fixed Point Iterative Method
+
+# Define functions of the form `x = g(x)` and plot them
+def g1(x):
+    return 0
+
+def g2(x):
+    return 0
+
+def g3(x):
+    return 0
+
+# Range of convergence
+def dg1(x):
+    return 0
+
+def dg2(x):
+    return 0
+
+def dg3(x):
+    return 0
+
+# Fixed-point iterative algorithm
+def fixed_point_iteration():
+    #==================================================================
+    # INPUTS
+    # -
+    # OUTPUTS
+    # -
+    #==================================================================
+
+    print('*** FIXED POINT ITERATION METHOD ***')
+    return 0
+
+# Test the algorithm and plot it
+
+
+# # Newton-Raphson Method
+
+# Newton-Raphson method algorithm
+def newton_raphson():
+    #==================================================================
+    # INPUTS
+    # -
+    # OUTPUTS
+    # -
+    #==================================================================
+
+    print('*** NEWTON-RAPHSON METHOD ***')
+    return 0
+
+# Visualize fixed-point function
+def g():
+    return 0
+
+# Range of convergence
+def dg():
+    return 0
+
+# Test the algorithm and plot it
+
+
+# # Compare Methods
+
+# Compute the roots of the function with each method
+
+# Plot the convergence for all methods
+
